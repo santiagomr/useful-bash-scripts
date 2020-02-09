1e001e72d45d7ec9279008e33f2b4762f96d3fbf
santiagomr <santiagomr4@gmail.com>
2020-02-09 13:10:59 -0300

    Minor details in simple_backupper.sh

--- simple_backupper.sh
+++ simple_backupper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ### @santiagomr
+### GNU GPL v3.0
 
 BKP_PATH=/var/backups
 LOG_PATH=/var/log/backup.log
@@ -12,7 +13,7 @@ DB_NAME=db_name
 APP_PATH=/var/www/my_app
 APP_NAME=my_app
 
-echo "$(date -R) *** STARTING THE BACKUP" > $LOG_PATH
+echo "$(date -R) *** STARTING BACKUP" >> $LOG_PATH
 
 echo "$(date -R) *** Starting DB dump" >> $LOG_PATH
 mysqldump --single-transaction -h $DB_HOST -u $DB_USER $DB_NAME | gzip > $BKP_PATH/$DB_NAME.$(date +%Y%m%d).sql.gz
@@ -20,7 +21,7 @@ mysqldump --single-transaction -h $DB_HOST -u $DB_USER $DB_NAME | gzip > $BKP_PA
 echo "$(date -R) *** Starting app backup" >> $LOG_PATH
 tar cfz $BKP_PATH/$APP_NAME.$(date +%Y%m%d).tar.gz $APP_PATH/
 
-echo "$(date -R) *** Starting backups clean" >> $LOG_PATH
+echo "$(date -R) *** Starting old backups clean" >> $LOG_PATH
 find $BKP_PATH/ -type f -mtime +7 -name '*.gz' -execdir rm {} \;
 
 echo "$(date -R) *** BACKUP FINISHED" >> $LOG_PATH
